c138422411a50bbd0ccd21db43fc1d5027b894dd
Sebastian Göttschkes <sebastian@blossom.io>
2016-02-29 14:09:14 +0100

    Testing #2

--- pear_blossom.py
+++ pear_blossom.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 def debug():
-    print 'Debug #1'
+    print 'Debugging #1'
 
 def debug2():
-    print 'Debug #3'
+    print 'Debugging #3'
 
 if __name__ == '__main__':
-    print 'Debug #0'
+    print 'Debugging #0'
     debug()
-    print 'Debug #2'
+    print 'Debugging #2'
     debug2()
